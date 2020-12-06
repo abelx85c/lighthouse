3a470eb9f4230673b1cdd6f7836e0f163d3ec15d
abelx85c <abelx85c@gmail.com>
2020-12-06 19:34:50 +0800

    install axios

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
-import Vue from 'vue'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 
