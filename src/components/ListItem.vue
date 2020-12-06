7e5ca3c3b6001b15b28af4ba3e674ab19a498a16
abelx85c <abelx85c@gmail.com>
2020-12-06 22:17:29 +0800

    add ListItem vuetify

--- src/components/ListItem.vue
+++ src/components/ListItem.vue
@@ -1,11 +1,23 @@
 <template>
-  <ol>
+  <!-- <ol>
     <li v-for="item in contents" :key="item.name">
       {{ item.name }}
       {{ item.address }}
       {{ item.phone }}
     </li>
-  </ol>
+  </ol> -->
+  <v-list two-line>
+    <template v-for="(item, index) in contents">
+      <v-list-item :key="item.name">
+        <v-list-item-content>
+          <v-list-item-title>{{ item.name }}</v-list-item-title>
+          <v-list-item-subtitle>{{ item.address }}</v-list-item-subtitle>
+          <v-list-item-subtitle>{{ item.phone }}</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+      <v-divider v-if="index < contents.length - 1" :key="index"></v-divider>
+    </template>
+  </v-list>
 </template>
 
 <script>
