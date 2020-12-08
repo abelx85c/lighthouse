557fe7d534685e4a06ef0101d87bbaf2253e325b
abelx85c <abelx85c@gmail.com>
2020-12-08 21:42:39 +0800

    add mark on map

--- src/components/MapItem.vue
+++ src/components/MapItem.vue
@@ -14,13 +14,18 @@ let openStreetMap = {};
 
 export default {
   name: "MapItem",
+  computed: {
+    contents() {
+      return this.$store.state.contents;
+    },
+  },
   mounted() {
     // ...
     openStreetMap = L.map("mapid", {
-      center: [25.042474, 121.513729],
-      // 可以嘗試改變 zoom 的數值
-      // 筆者嘗試後覺得 18 的縮放比例是較適當的查詢範圍
-      zoom: 18,
+      //中心點設在中興大學
+      center: [24.123958, 120.677193],
+      //zoom縮放比例
+      zoom: 11,
     });
 
     L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
@@ -28,6 +33,20 @@ export default {
         'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
       maxZoom: 20,
     }).addTo(openStreetMap);
+
+    this.$store.dispatch("CONTENTS_READ");
+    this.markMap();
+  },
+
+  methods: {
+    markMap() {
+      //在中興大學設一個標記
+      //L.marker([24.123958, 120.677193]).addTo(openStreetMap);
+      this.contents.forEach((item) => {
+        //透過經緯度疊加標記
+        L.marker([item.latitude, item.longitude]).addTo(openStreetMap);
+      });
+    },
   },
 };
 </script>
