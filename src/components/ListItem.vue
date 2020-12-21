842359322a558330b0d37d4984f7096ff0507c34
abelx85c <abelx85c@gmail.com>
2020-12-21 16:19:50 +0800

    modify ListItem.vue center align

--- src/components/ListItem.vue
+++ src/components/ListItem.vue
@@ -6,18 +6,25 @@
       {{ item.phone }}
     </li>
   </ol> -->
-  <v-list two-line>
-    <template v-for="(item, index) in contents">
-      <v-list-item :key="item.name">
-        <v-list-item-content>
-          <v-list-item-title>{{ item.name }}</v-list-item-title>
-          <v-list-item-subtitle>{{ item.address }}</v-list-item-subtitle>
-          <v-list-item-subtitle>{{ item.phone }}</v-list-item-subtitle>
-        </v-list-item-content>
-      </v-list-item>
-      <v-divider v-if="index < contents.length - 1" :key="index"></v-divider>
-    </template>
-  </v-list>
+  <div class="wrap">
+    <div class="list">
+      <v-list>
+        <template v-for="(item, index) in contents">
+          <v-list-item :key="item.name">
+            <v-list-item-content>
+              <v-list-item-title>{{ item.name }}</v-list-item-title>
+              <v-list-item-subtitle>{{ item.address }}</v-list-item-subtitle>
+              <v-list-item-subtitle>{{ item.phone }}</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+          <v-divider
+            v-if="index < contents.length - 1"
+            :key="index"
+          ></v-divider>
+        </template>
+      </v-list>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -35,3 +42,14 @@ export default {
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.wrap {
+  margin: auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .list {
+  }
+}
+</style>
