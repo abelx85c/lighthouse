6a4b195dd0115d2a6cca5b771beff7e89be1cc46
abelx85c <abelx85c@gmail.com>
2020-12-21 16:38:28 +0800

    modify a wrong word

--- src/components/MapItem.vue
+++ src/components/MapItem.vue
@@ -47,7 +47,7 @@ export default {
       //在中興大學設一個標記
       //L.marker([24.123958, 120.677193]).addTo(openStreetMap);
       this.contents.forEach((item) => {
-        //透過經緯度標記並點選後嗅出資訊
+        //透過經緯度標記並點選後秀出資訊
         L.marker([item.latitude, item.longitude]).addTo(openStreetMap)
           .bindPopup(`<p><strong style="font-size: 20px;">${item.name}</strong></p>
           地址: ${item.address}<br>
