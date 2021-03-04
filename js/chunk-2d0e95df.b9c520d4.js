7ca2877e5495666cc1cd815e045eeeb6ae3a6de1
abelx85c <abelx85c@gmail.com>
2021-03-04 15:00:32 +0800

    deploy

---
+++ js/chunk-2d0e95df.b9c520d4.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0e95df"],{"8cdb":function(e,n,t){"use strict";t.r(n);var u=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[t("h1",[e._v("404 找不到網頁")]),t("router-view")],1)},c=[];console.log("@/views/404.vue");var l={},o=l,r=t("2877"),s=Object(r["a"])(o,u,c,!1,null,null,null);n["default"]=s.exports}}]);
+//# sourceMappingURL=chunk-2d0e95df.b9c520d4.js.map
